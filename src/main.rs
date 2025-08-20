2ac10d4589ebb2046cd42f86faabb0206734fe54
Bojan Kartal <boankartal@gmail.com>
2025-08-21 00:06:57 +0300

    feat: write basic wofi launcher with test data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::io::Write;
+use anyhow::Result;
+use std::collections::HashMap;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let mut hm = HashMap::new();
+    hm.insert("firefox", "e");
+    hm.insert("dolphin", "dolphin");
+    let mut wofi = Command::new("wofi")
+        .arg("--dmenu")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("sdls");
+
+    let mut input = String::new();
+    for (key, _) in hm.iter() {
+        input = input + &format!("{}\n", key);
+    }
+    wofi.stdin.as_mut().unwrap().write_all(input.trim().as_bytes()).expect("slkd");
+    let result = wofi.wait_with_output().expect("sldls");
+    let com = hm.get(String::from_utf8(result.stdout)?.trim()).unwrap();
+    let com_arr: Vec<&str> = com.split(" ").collect();
+    let program = &com_arr.get(0).unwrap();
+    let args = &com_arr[1..];
+    
+    Command::new(&program).args(args).spawn()?;
+
+    Ok(())
 }
