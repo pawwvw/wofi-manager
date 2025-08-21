41f724c2e7149f6b349e98a4a52f0cf4259dc71b
Bojan Kartal <boankartal@gmail.com>
2025-08-21 09:57:15 +0300

    feat: add JSON configuration support for wofi launcher

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use std::process::{Command, Stdio};
 use std::io::Write;
 use anyhow::Result;
-use std::collections::HashMap;
+use clap::Parser;
+use std::fs;
+use serde_json::Value;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    options: String
+}
 
 fn main() -> Result<()> {
-    let mut hm = HashMap::new();
-    hm.insert("firefox", "e");
-    hm.insert("dolphin", "dolphin");
+
+    let args = Args::parse();
+
+    let options_path = args.options;
+
+    let options_file = fs::read_to_string(options_path)?;
+
+    let options_json: Value = serde_json::from_str(&options_file)?;
+
+    let options = options_json.as_object().unwrap();
     let mut wofi = Command::new("wofi")
         .arg("--dmenu")
         .stdin(Stdio::piped())
@@ -15,12 +32,12 @@ fn main() -> Result<()> {
         .expect("sdls");
 
     let mut input = String::new();
-    for (key, _) in hm.iter() {
+    for (key, _) in options.iter().rev() {
         input = input + &format!("{}\n", key);
     }
     wofi.stdin.as_mut().unwrap().write_all(input.trim().as_bytes()).expect("slkd");
     let result = wofi.wait_with_output().expect("sldls");
-    let com = hm.get(String::from_utf8(result.stdout)?.trim()).unwrap();
+    let com = options.get(String::from_utf8(result.stdout)?.trim()).unwrap().as_str().unwrap_or("").to_string();
     let com_arr: Vec<&str> = com.split(" ").collect();
     let program = &com_arr.get(0).unwrap();
     let args = &com_arr[1..];
